a6de8a9757f386899152a0b115214ebb50abd666
Atlas Cove <Atlas48@gmx.com>
2021-12-24 16:40:15 +0000

    Update ampelstatus.ts
    Made ordering more consistent.

--- ampelstatus.ts
+++ ampelstatus.ts
@@ -1,5 +1,5 @@
-export var wrn: string="\x1B[1;93mWRN\x1B[0m: ";
 export var inf: string="\x1B[1;32mINF\x1B[0m: ";
+export var wrn: string="\x1B[1;93mWRN\x1B[0m: ";
 export var err: string="\x1B[1;31mERR\x1B[0m: ";
 export enum status {
     inf,
